a223bc194a4490d62ff3a0c3bc39aa2f4d47336c
Kelvin Kang <0degreekelvin@gmail.com>
2020-02-05 11:49:10 +0800

    Removing more TODO

--- include/format.h
+++ include/format.h
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace Format {
-std::string ElapsedTime(const long seconds);  // TODO: See src/format.cpp
+std::string ElapsedTime(const long seconds);
 std::string AppendZero(const long number);
 };  // namespace Format
 
